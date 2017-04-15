191732555f2e6f9556bae676e0c3263b94171166
rikknijff <Rik.Knijff@rhea.infosupport.net>
2017-04-15 20:24:58 +0200

    first commit

---
+++ build/classes-generated/main/Bundle.java
@@ -0,0 +1,27 @@
+package main;
+/** Localizable strings for {@link main}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Spotify</i>
+     * @see SpotifyAlbumSearch
+     */
+    static String CTL_klsdAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_klsdAction");
+    }
+    /**
+     * @return <i>Spotify Album Search</i>
+     * @see SpotifyAlbumSearch
+     */
+    static String CTL_klsdTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_klsdTopComponent");
+    }
+    /**
+     * @return <i>Spotify Album Search</i>
+     * @see SpotifyAlbumSearch
+     */
+    static String HINT_klsdTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_klsdTopComponent");
+    }
+    private Bundle() {}
+}
